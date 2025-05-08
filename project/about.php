05f3868151b3a0d0a976ff811bb80bda981e239b
Swostika Shrestha <shrestha.swosti1271@gmail.com>
2025-05-08 11:18:43 +0545

    Add project

---
+++ project/about.php
@@ -0,0 +1,64 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>about us</h3>
+   <p> <a href="home.php">Home</a> / About </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/aboutus.png" alt="">
+      </div>
+
+      <div class="content">
+         <h3>The book house</h3>
+         <p>Welcome to The Book House, an online bookstore with its own identity and management style dedicated to the books. At the book house you can browse and buy books online. With an aim to create an easy environment for book readers, we deliver books at your doorstep. </p>
+         <p>We are physically based in Kathmandu, Nepal but we deliver all over the country.</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
